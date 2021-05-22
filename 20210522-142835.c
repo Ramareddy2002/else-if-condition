cdb443efc1bba8f88e47dea6bca60957535d635f
Ramareddy2002 <84448206+Ramareddy2002@users.noreply.github.com>
2021-05-22 14:29:27 +0530

    Add files via upload

---
+++ 20210522-142835.c
@@ -0,0 +1,30 @@
+num=int(raw_input())
+if(num%2==0):
+ print("divisible by 2")
+if(num%3==0): 
+print("divisible by 3")
+if(num%4==0): 
+print("divisible by 4")
+if(num%5==0):
+ print("divisible by 5")
+else: 
+print("number is not divisible by 2,3,4,5")
+
+
+'''
+expected output
+20
+divisible by 2
+case 2
+3
+divisible by 3
+case 3 
+16 
+divisible by 4
+case 4
+25
+divisible by 5
+case 5
+37
+number not divisible by 2,3,4,5
+'''
